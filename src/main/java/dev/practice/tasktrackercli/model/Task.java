b5765a77bd967203161cc0faecde14413d22917c
Kelvin <kelvintawiah224@gmail.com>
2025-02-18 06:01:23 -0600

    setup task model

---
+++ src/main/java/dev/practice/tasktrackercli/model/Task.java
@@ -0,0 +1,73 @@
+package dev.practice.tasktrackercli.model;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private static long idCounter = 1;
+
+    private Long id;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(String description,TaskStatus status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = idCounter++;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
---
+++ src/main/java/dev/practice/tasktrackercli/model/TaskStatus.java
@@ -0,0 +1,7 @@
+package dev.practice.tasktrackercli.model;
+
+public enum TaskStatus {
+    NOT_STARTED,
+    IN_PROGRESS,
+    COMPLETED
+}
