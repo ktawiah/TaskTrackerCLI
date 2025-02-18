63558573b370679ee4cf2c485ae24d5f33699ed7
Kelvin <kelvintawiah224@gmail.com>
2025-02-18 06:00:37 -0600

    setup shell project

---
+++ src/test/java/dev/practice/tasktrackercli/TaskTrackerCliApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.tasktrackercli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
